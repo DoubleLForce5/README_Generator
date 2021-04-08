e70122ef562bfd6eb346200b93b744ec04ef73aa
Liv <lopez.olivia17@outlook.com>
2021-04-08 17:59:05 -0500

    adjusted generateMarkdown function by adding template literals

--- Develop/index.js
+++ Develop/index.js
@@ -1,44 +1,44 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const utils = require('utils');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = () => {
   return inquirer.prompt([
     {
       type: 'input',
-      name: 'project name',
+      name: 'title',
       message: 'What is the name of your project?'
     },
     {
       type: 'input',
-      name: 'project description',
+      name: 'description',
       message: 'Project description?'
     },
     {
       type: 'input',
-      name: 'project installation instructions',
+      name: 'instructions',
       message: 'Any installation instructions?'
     },
     {
       type: 'input',
-      name: 'project usage information',
+      name: 'information',
       message: 'Usage information?'
     },
     {
       type: 'input',
-      name: 'project contributors',
+      name: 'contributors',
       message: 'List any known project contributors and how others can contribute going forward.'
     },
     {
       type: 'input',
-      name: 'project test instructions',
+      name: 'test',
       message: 'If you have them, please provide test examples for your project and how others can run them.'
     },
     {
-      type: 'list ',
-      name: 'project licensing options',
+      type: 'list',
+      name: 'licensing',
       message: 'Please select a license for your project:',
       choices: [
         'MIT License',
@@ -48,28 +48,29 @@ const questions = () => {
     },
     {
       type: 'input',
-      name: 'github username',
+      name: 'github',
       message: 'Github username?'
     },
     {
       type: 'input',
-      name: 'email address',
+      name: 'email',
       message: 'Email Address?'
     }
   ]);
 }; 
 
 // TODO: Create a function to write README file
-
-fs.writeFile(`README.md`, generateMarkdown, (err) =>{
-  if(err) throw err;
-  console.log(`file saved`)
-});
+function writeToFile(fileName, data) { 
+  fs.writeFile(fileName, data, (error) => {
+    if (error) throw error;
+    console.log('file saved')
+  });
+};
 
 // TODO: Create a function to initialize app
 function init() {
   questions ()
-    .then ((data) => writeFileAsync('README.md', generateMarkdown(data)))
+    .then ((data) => writeToFile('README.md', generateMarkdown(data)))
     .then (() => console.log('Successfully wrote to README.md'))
     .catch((err) => console.error(err));
 };
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -29,7 +29,14 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+## ${data.description}
+## ${data.instructions}
+##${data.information}
+##${data.contributors}
+##${data.test}
+##${data.licensing}
+##${data.github}
+##${data.email}
 `;
 }
 
